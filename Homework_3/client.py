057b993b0a03b1159a199925fde1cf82e0ace333
Dmytrii Basov <dbasov@securecodewarrior.com>
2024-08-14 23:06:03 +0300

    Add WeatherServer and Client classes.

---
+++ Homework_3/client.py
@@ -0,0 +1,42 @@
+import asyncio
+
+
+class WeatherServerClient:
+
+    def __init__(self, host='localhost', port=8000):
+        self.host = host
+        self.port = port
+
+    async def send_message(self, writer: asyncio.StreamWriter, message: str):
+        print(f'Sending message: {message}')
+        msg_bytes = message.encode()
+        writer.write(msg_bytes)
+        await writer.drain()
+
+    async def read_response(self, reader: asyncio.StreamReader):
+        print('Waiting for response...')
+        msg = await reader.readline()
+        response = msg.decode()
+        print(f'Server responded: {response}')
+
+    async def run_client(self):
+        message = "What is the weather today?"
+        reader, writer = await asyncio.open_connection(self.host, self.port)
+        print('Connected')
+        await self.send_message(writer, message)
+        await self.read_response(reader)
+        writer.close()
+        await writer.wait_closed()
+
+
+    # async def main():
+    #     message = "What is the weather today?"
+    #     reader, writer = await asyncio.open_connection("localhost", 8000)
+    #     print('Connected')
+    #     await send_message(writer, message)
+    #     await read_response(reader)
+    #     writer.close()
+    #     await writer.wait_closed()
+
+
+asyncio.run(WeatherServerClient().run_client())
---
+++ Homework_3/server.py
@@ -0,0 +1,51 @@
+import asyncio
+from random import randint
+
+
+class BroadcastWeatherServer:
+
+    def __init__(self, host='localhost', port=8000):
+        self.host = host
+        self.port = port
+        self.clients = set()
+
+    async def handler(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        self.clients.add(writer)
+
+        try:
+            await asyncio.sleep(30)
+        except asyncio.CancelledError:
+            pass
+        finally:
+            self.clients.remove(writer)
+            writer.close()
+            await writer.wait_closed()
+
+    async def broadcast_weather(self):
+        while True:
+            await asyncio.sleep(10)
+            message = f"temp = {randint(-10, 40)} Celsius degree\n"
+            print(message)
+            for client in list(self.clients):
+
+                try:
+                    client.write(message.encode())
+                    await client.drain()
+                    client.close()
+                    await client.wait_closed()
+                    self.clients.remove(client)
+
+                except Exception as e:
+                    print(f'Failed to send to the client')
+                    self.clients.remove(client)
+
+    async def start_server(self):
+        server = await asyncio.start_server(self.handler, self.host, self.port)
+        async with server:
+            await asyncio.gather(
+                server.serve_forever(),
+                self.broadcast_weather()
+            )
+
+
+asyncio.run(BroadcastWeatherServer().start_server())
